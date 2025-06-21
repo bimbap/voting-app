09cdb43bf5d74ec48434c68e3e08adbd9757cd62
Vynzx <tama335544tama@gmail.com>
2025-06-21 22:39:28 +0700

    Biggy Changes 2

--- worker/Program.cs
+++ worker/Program.cs
@@ -16,7 +16,7 @@ namespace Worker
         {
             try
             {
-                var pgsql = OpenDbConnection("Server=lksn-db.caa35omlxgeu.us-east-1.rds.amazonaws.com;Username=admin;Password=LKSNCC2024;Database=lksndb2024");
+                var pgsql = OpenDbConnection("Server=lksn-db.caa35omlxgeu.us-east-1.rds.amazonaws.com;Username=postgres;Password=LKSNCC2024;Database=lksndb2024");
                 var options = new ConfigurationOptions
                     {
                         EndPoints = { "master.lks-redis.p7qul5.use1.cache.amazonaws.com:6379" },
